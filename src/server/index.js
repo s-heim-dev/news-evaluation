56fa8a46230044358eb80ae181afa8f5fe72c6ea
Susanne Heim <s-heim@mailbox.org>
2023-04-18 18:42:33 +0200

    Initial Commit

---
+++ src/server/index.js
@@ -0,0 +1,23 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+const port = 8080;
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
